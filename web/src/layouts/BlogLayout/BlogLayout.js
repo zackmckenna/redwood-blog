766cd424bdea2c3efe24d502add0dae1259b19da
Zack McKenna <zackmckenna@gmail.com>
2020-06-18 17:10:48 -0400

    added auth

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,6 +1,9 @@
 import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
+  const { logIn, isAuthenticated, logOut, currentUser } = useAuth()
+
   return (
     <div>
       <h1>
@@ -13,6 +16,11 @@ const BlogLayout = ({ children }) => {
           </li>
           <li>
             <Link to={routes.contact()}>Contact</Link>
+          </li>
+          <li>
+            <a href="#" onClick={isAuthenticated ? logOut : logIn}>
+              {isAuthenticated ? 'Log Out' : 'Log In'}
+            </a>
           </li>
         </ul>
       </nav>
